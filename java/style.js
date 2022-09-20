6a5288a6bbe0cc6d033ed043d30a336d781b4b9d
WaqasMalok <waqasimalok@gmail.com>
2022-09-20 18:05:46 +0500

    complet

--- java/style.js
+++ java/style.js
@@ -1,55 +1,69 @@
-function show_btn() {
-    let show = document.getElementById("show");
-    let hide = document.getElementById("hide");
-    let navmb = document.getElementById("navmb"); 
-    if (navmb.style.display = !"block") {
-        navmb.style.display = "block";
+window.addEventListener("scroll", function () {
+    let nav = document.querySelector("header");
+    nav.classList.toggle("navbg", window.scrollY > 0);
 
-    } else {
-        navmb.style.display = "block";
-        show.style.display = "none";
-        hide.style.display = "block";
-    }
+});
+// function show_btn() {
+//     let show = document.getElementById("show");
+//     let hide = document.getElementById("hide");
+//     let navmb = document.getElementById("navimb"); 
+//     if (navmb.style.display = !"block") {
+//         navmb.style.display = "block";
 
-}
-function hide_btn() {
-    let show = document.getElementById("show");
-    let hide = document.getElementById("hide");
-    let navmb = document.getElementById("navmb");
-    if (navmb.style.display = !"none") {
-        navmb.style.display = "none";
+//     } else {
+//         navmb.style.display = "block";
+//         show.style.display = "none";
+//         hide.style.display = "block";
+//     }
 
-    } else {
-        navmb.style.display = "none";
-        show.style.display = "block";
-        hide.style.display = "none";
-    }
+// }
 
-}
+ show = document.getElementById("show");
+ hide = document.getElementById("hide");
+ navmb = document.getElementById("navimb");
+navmb.addEventListener('clik', ()=>{
+    navmb.classList.toggle("navmbscroll");
+    navmb.classList.toggle("navmbscroll");
+})
 
- function  li_a() {
+
+
+// function hide_btn() {
+//     let show = document.getElementById("show");
+//     let hide = document.getElementById("hide");
+//     let navmb = document.getElementById("navimb");
+//     if (navmb.style.display = !"none") {
+//         navmb.style.display = "none";
+
+//     } else {
+//         navmb.style.display = "none";
+//         show.style.display = "block";
+//         hide.style.display = "none";
+//     }
+
+// }
+
+function lia() {
     let lia = document.getElementsByClassName("li");
-    let navii = document.getElementById("navmb");
     let show = document.getElementById("show");
     let hide = document.getElementById("hide");
-    let navmb = document.getElementById("navmb"); 
+    let navmb = document.getElementById("navi");
+    let body = document.querySelector("body");
     if (navmb.style.display = !"none") {
         navmb.style.display = "none"
-    }else {
+    } else {
         navmb.style.display = "none";
         show.style.display = "block";
         hide.style.display = "none";
     }
- }
+}
+function body() {
+    let body = document.querySelector("body");
+    if (navmb.style.display = !"none") {
+        navmb.style.display = "none"
+    } else {
+        navmb.style.display = "none";
+    }
+}
 
 
-
-
-
-// function atag() {
-//     let a = document.getElementById("a");
-//     let navi = document.getElementById("navi");
-//     if (navi.style.display = 'block') {
-//         navi.style.display = "none";
-//     }
-// }
